c85f96e9a9e22d2777054d06fd937fb4de96ccd5
Howard <howard@HowarddeMacBook-Pro.local>
2018-10-25 16:37:03 +0800

    add conditional_DCGAN for mnist

--- Conditional_DCGAN.py
+++ Conditional_DCGAN.py
@@ -1,5 +1,5 @@
 '''
-Architecture guidelines for stable Deep Convolutional GANs
+Architecture guidelines for stable Deep Convolutional GANs :
 
 Replace any pooling layers with strided convolutions(discriminator) and fractional - strided convolutions(generator).
 Use batchnorm in both the generator and the discriminator
@@ -32,19 +32,38 @@ def sample_Z(m, n):
     return np.random.uniform(-1., 1., size=[m, n])
 
 
+def show_Y(m, n, index):
+    y = np.zeros([m, n])
+    for i in range(m):
+        y[i][index] = 1
+    return y
+
+
+def concat(z, y):
+    return tf.concat([z, y], 1)
+
+
+def conv_concat(x, y):
+    batch_size = tf.shape(x)[0]
+    y = tf.reshape(y, [batch_size, 1, 1, 10])
+    print(tf.concat([x, y * tf.ones([batch_size, 28, 28, 10])], 3))
+    return tf.concat([x, y * tf.ones([batch_size, 28, 28, 10])], 3)
+
+
 X = tf.placeholder(tf.float32, [None, 28, 28, 1])
 Z = tf.placeholder(tf.float32, [None, 62])
+Y = tf.placeholder(tf.float32, [None, 10])
 
 w_init = tf.random_normal_initializer(mean=0, stddev=0.02)
 b_init = tf.constant_initializer(0)
 
 
-def generator():
+def generator(z):
     with tf.variable_scope("generator"):
         # z = [batch,62]
         # G_dense1 = slim.fully_connected(Z, 1024, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
         # G_dense1 = [batch,1024]
-        G_dense2 = slim.fully_connected(Z, 128 * 7 * 7, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
+        G_dense2 = slim.fully_connected(z, 128 * 7 * 7, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
         # G_dense1 = [batch,128 * 7 * 7]
         G_dense2_flatt = tf.reshape(G_dense2, [-1, 7, 7, 128])
         G_deconv1 = slim.conv2d_transpose(G_dense2_flatt, 64, [4, 4], 2, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
@@ -59,9 +78,9 @@ def generator():
 def discriminator(input, reuse=False):
     with tf.variable_scope("discriminator", reuse=reuse):
         # input = [batch,28,28,1]
-        D_conv1 = slim.conv2d(input, 64, [4, 4], 2, padding="SAME", activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
+        D_conv1 = slim.conv2d(input, 64, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, biases_initializer=b_init)
         # D_conv1 = [batch,14,14,64]
-        D_conv2 = slim.conv2d(D_conv1, 128, [4, 4], 2, padding="SAME", activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        D_conv2 = slim.conv2d(D_conv1, 128, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
         # D_conv1 = [batch,7,7,128]
         flat_size = D_conv2.shape[1] * D_conv2.shape[2] * D_conv2.shape[3]
         D_conv2_flat = tf.reshape(D_conv2, [-1, int(flat_size)])
@@ -89,9 +108,9 @@ def plot(samples):
     return fig
 
 
-G_sample = generator()
-D_logits_real, _ = discriminator(X)
-D_logits_fake, _ = discriminator(G_sample, reuse=True)
+G_sample = generator(concat(Z, Y))
+D_logits_real, _ = discriminator(conv_concat(X, Y))
+D_logits_fake, _ = discriminator(conv_concat(G_sample, Y), reuse=True)
 
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits_real, labels=tf.ones_like(D_logits_real)))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits_fake, labels=tf.zeros_like(D_logits_fake)))
@@ -127,21 +146,24 @@ with tf.Session(config=gpuConfig) as sess:
     Z_dim = 62
 
     z_show = sample_Z(16, Z_dim)
+    y_show = show_Y(16, 10, index=3)
+
     for it in range(1000000):
         if it % 100 == 0:
-            samples = sess.run(G_sample, feed_dict={Z: z_show})
+            samples = sess.run(G_sample, feed_dict={Y: y_show, Z: z_show})
 
             fig = plot(samples)
             plt.savefig(dir_name + '/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
             i += 1
             plt.close(fig)
 
-        X_mb, _ = mnist.train.next_batch(mb_size)
+        X_mb, Y_mb = mnist.train.next_batch(mb_size)
         X_mb = [x * 2 - 1 for x in X_mb]
+        # print(Y[0])
         # print(X_mb[0])
         X_mb = np.reshape(X_mb, [-1, 28, 28, 1])
-        _, D_loss_curr = sess.run([D_optimizer, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
-        _, G_loss_curr = sess.run([G_optimizer, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim)})
+        _, D_loss_curr = sess.run([D_optimizer, D_loss], feed_dict={X: X_mb, Y: Y_mb, Z: sample_Z(mb_size, Z_dim)})
+        _, G_loss_curr = sess.run([G_optimizer, G_loss], feed_dict={Y: Y_mb, Z: sample_Z(mb_size, Z_dim)})
 
         if it % 100 == 0:
             print('Iter: {}'.format(it))
