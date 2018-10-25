9ad5274e44e782c380a572462baa0aac5cb2abfc
Howard <howard@HowarddeMacBook-Pro.local>
2018-10-25 15:48:32 +0800

    update

--- DCGAN_bottle.py
+++ DCGAN_bottle.py
@@ -10,11 +10,11 @@ Use LeakyReLU activation in the discriminator for all layers.
 '''
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from datas import get_batch
 
 
 def xavier_init(size):
@@ -27,40 +27,49 @@ def sample_Z(m, n):
     return np.random.uniform(-1., 1., size=[m, n])
 
 
-X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-Z = tf.placeholder(tf.float32, [None, 62])
+X = tf.placeholder(tf.float32, [None, 64, 64, 3])
+Z = tf.placeholder(tf.float32, [None, 100])
 
 w_init = tf.random_normal_initializer(mean=0, stddev=0.02)
 b_init = tf.constant_initializer(0)
 
 
-def generator():
+def generator(is_training=True):
     with tf.variable_scope("generator"):
         # z = [batch,62]
-        # G_dense1 = slim.fully_connected(Z, 1024, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
+        # G_dense1 = slim.fully_connected(Z, 4 * 4 * 1024, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
         # G_dense1 = [batch,1024]
-        G_dense2 = slim.fully_connected(Z, 128 * 7 * 7, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
-        # G_dense1 = [batch,128 * 7 * 7]
-        G_dense2_flatt = tf.reshape(G_dense2, [-1, 7, 7, 128])
-        G_deconv1 = slim.conv2d_transpose(G_dense2_flatt, 64, [4, 4], 2, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
-        # G_deconv1 = [batch,14,14,64]
-        G_deconv2 = slim.conv2d_transpose(G_deconv1, 1, [4, 4], 2, activation_fn=None, weights_initializer=w_init, biases_initializer=b_init)
-        # G_deconv2 = [batch,28,28,1]
-        G_logits = G_deconv2
-        G_prob = tf.nn.tanh(G_logits)
+        G_dense2 = slim.fully_connected(Z, 1024 * 4 * 4, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # G_dense1 = [batch,1024 * 4 * 4]
+        G_dense2_flatt = tf.reshape(G_dense2, [-1, 4, 4, 1024])
+        G_deconv1 = slim.conv2d_transpose(G_dense2_flatt, 512, [3, 3], 2, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # G_deconv1 = [batch,8,8,512]
+        G_deconv2 = slim.conv2d_transpose(G_deconv1, 256, [3, 3], 2, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # G_deconv1 = [batch,16,16,256]
+        G_deconv3 = slim.conv2d_transpose(G_deconv2, 128, [3, 3], 2, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # G_deconv1 = [batch,32,32,128]
+        G_deconv4 = slim.conv2d_transpose(G_deconv3, 3, [3, 3], 2, activation_fn=tf.nn.tanh, weights_initializer=w_init, biases_initializer=b_init)
+        # G_deconv2 = [batch,64,64,3]
+        print('G_deconv4 ', G_deconv4)
+        G_prob = G_deconv4
         return G_prob
 
 
-def discriminator(input, reuse=False):
+def discriminator(input, reuse=False, is_training=True):
     with tf.variable_scope("discriminator", reuse=reuse):
-        # input = [batch,28,28,1]
-        D_conv1 = slim.conv2d(input, 64, [4, 4], 2, padding="SAME", activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init)
-        # D_conv1 = [batch,14,14,64]
-        D_conv2 = slim.conv2d(D_conv1, 128, [4, 4], 2, padding="SAME", activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
-        # D_conv1 = [batch,7,7,128]
+        # input = [batch,64,64,3]
+        D_conv1 = slim.conv2d(input, 64, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, biases_initializer=b_init)
+        # D_conv1 = [batch,32,32,64]
+        D_conv2 = slim.conv2d(D_conv1, 64 * 2, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # D_conv2 = [batch,16,16,128]
+        D_conv3 = slim.conv2d(D_conv2, 64 * 4, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # D_conv2 = [batch,8,8,256]
+        D_conv3 = slim.conv2d(D_conv3, 64 * 8, [4, 4], 2, padding="SAME", activation_fn=tf.nn.leaky_relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
+        # D_conv2 = [batch,4,4,512]
+        print('D_conv3', D_conv3)
         flat_size = D_conv2.shape[1] * D_conv2.shape[2] * D_conv2.shape[3]
         D_conv2_flat = tf.reshape(D_conv2, [-1, int(flat_size)])
-        # D_dense1 = slim.fully_connected(D_conv2_flat, 1024, activation_fn=tf.nn.relu, weights_initializer=w_init, biases_initializer=b_init, reuse=reuse, scope="d_dense1")
+        # D_dense1 = slim.fully_connected(D_conv2_flat, 1024, activation_fn=tf.nn.relu, weights_initializer=w_init, normalizer_fn=slim.batch_norm)
         D_dense2 = slim.fully_connected(D_conv2_flat, 1, activation_fn=None, weights_initializer=w_init, biases_initializer=b_init)
         # D_dense2 = [batch,1]
         D_logits = D_dense2
@@ -79,7 +88,7 @@ def plot(samples):
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_aspect('equal')
-        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+        plt.imshow(sample)
 
     return fig
 
@@ -98,13 +107,14 @@ t_var = tf.trainable_variables()
 theta_D = [var for var in t_var if 'discriminator' in var.name]
 theta_G = [var for var in t_var if 'generator' in var.name]
 
+theta_D_new = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
 print("theta_D   ", theta_D)
-print("theta_G   ", theta_G)
+print("theta_D_new   ", theta_D_new)
 # print(theta_G)
+
+
 D_optimizer = tf.train.AdamOptimizer(learning_rate=2e-4).minimize(D_loss, var_list=theta_D)  # note:only update D gradient, use var_list
 G_optimizer = tf.train.AdamOptimizer(learning_rate=2e-4).minimize(G_loss, var_list=theta_G)  # note:only update G gradient, use var_list
-
-mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
@@ -119,22 +129,20 @@ with tf.Session(config=gpuConfig) as sess:
 
     i = 0
     mb_size = 128
-    Z_dim = 62
+    Z_dim = 100
 
     z_show = sample_Z(16, Z_dim)
     for it in range(1000000):
         if it % 100 == 0:
             samples = sess.run(G_sample, feed_dict={Z: z_show})
-
+            samples = (samples + 1.) / 2.  # inverse transform from [-1,1] to [0,1]
             fig = plot(samples)
             plt.savefig(dir_name + '/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
             i += 1
             plt.close(fig)
 
-        X_mb, _ = mnist.train.next_batch(mb_size)
-        X_mb = [x * 2 - 1 for x in X_mb]  # transform from [0,1] to [-1,1] for generator tanh
-        # print(X_mb[0])
-        X_mb = np.reshape(X_mb, [-1, 28, 28, 1])
+        X_mb = get_batch(mb_size, resize=True)
+        # X_mb = np.reshape(X_mb, [-1, 28, 28, 1])
         _, D_loss_curr = sess.run([D_optimizer, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
         _, G_loss_curr = sess.run([G_optimizer, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim)})
 
